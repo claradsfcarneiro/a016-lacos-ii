f0a824eac563a9bd2b84ebb8f2e426ca23e54f62
Clara <claradsfcarneiro@gmail.com>
2022-09-14 16:51:03 -0300

    Realizar exercício 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,16 @@
-const array = [
+const gols = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
     [5, 5, 12, 16, 4],
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for (let i in gols) {
+    let golsJogador = `Jogador ${Number(i)+1}: `;
+    for (let j of gols[i]) {
+        golsJogador += j + ", "
+    }
+    golsJogador = golsJogador.slice(0, -2);
+    console.log(golsJogador);
+}
